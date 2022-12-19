d29c919c1e97d04f5daea83ed5155bd0eca67360
Viktor <viktor_23_08@mail.ru>
2022-12-19 16:09:39 +0100

    homework 12

---
+++ src/pro/sky/java/homework12/Author.java
@@ -0,0 +1,17 @@
+package pro.sky.java.homework12;
+
+public class Author {
+    private String authorName;
+    private String authorLastName;
+
+    public Author(String authorName, String authorLastName) {
+        this.authorLastName = authorLastName;
+        this.authorName = authorName;
+    }
+
+    public String getAuthorFullName() {
+        return authorName + " " + authorLastName;
+    }
+
+
+}
---
+++ src/pro/sky/java/homework12/Book.java
@@ -0,0 +1,29 @@
+package pro.sky.java.homework12;
+
+public class Book {
+    private Author authorName;
+    private String bookName;
+    private int publishingYear;
+
+    public Book(Author authorName, String bookName, int publishingYear) {
+        this.authorName = authorName;
+        this.bookName = bookName;
+        this.publishingYear = publishingYear;
+    }
+
+    public Author getAuthorName() {
+        return authorName;
+    }
+
+    public String getBookName() {
+        return bookName;
+    }
+
+    public int getPublishingYear() {
+        return publishingYear;
+    }
+
+    public void setPublishingYear(int publishingYear) {
+        this.publishingYear = publishingYear;
+    }
+}
